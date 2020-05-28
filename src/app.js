95c165867355d72372029d4ab3e5712ebbe7cf23
Joseph Terschak <jterschak@gmail.com>
2020-05-28 22:51:20 +0900

    module 3

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 
 const app = express();
 app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -31,6 +32,31 @@ app.get('/credit', function (req, res) {
 app.get('/profile', function (req, res) {
   res.render('profile', {user: users[0]});
 });
+
+app.get('/transfer', function (req, res) {
+  res.render('transfer');
+});
+
+app.post('/transfer', function (req, res) {
+  accounts[req.body.from].balance = accounts[req.body.from].balance - parseInt(req.body.amount);
+  accounts[req.body.to].balance = accounts[req.body.to].balance + parseInt(req.body.amount);
+  const accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(path.join(__dirname, '/json/accounts.json'), accountsJSON, "UTF8");
+  res.render('transfer', {message: "Transfer Completed"})
+});
+
+app.get('/payment', function (req, res) {
+  res.render('payment', {account: accounts.credit});
+});
+
+app.post('/payment', function (req, res) {
+  accounts.credit.balance = accounts.credit.balance - req.body.amount;
+  accounts.credit.available = accounts.credit.available + parseInt(req.body.amount);
+  const accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(path.join(__dirname, '/json/accounts.json'), accountsJSON, "UTF8");
+  res.render('payment', { message: "Payment Successful", account: accounts.credit })
+});
+
 
 const server = app.listen(3000, function () {
   console.log(`app running on port ${server.address().port}`);
